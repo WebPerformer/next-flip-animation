fd702a457df765dcad8f4fbed6af98addbbaf0c2
WebPerformer <webperformerz@gmail.com>
2023-04-14 19:38:59 -0300

    Update Head

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({ children }) {
   return (
     <div className="layout">
         <Head>
-          <title>Next.js Template</title>
+          <title>Flip Animation</title>
         </Head>
         <header></header>
         <main className="main-container">
