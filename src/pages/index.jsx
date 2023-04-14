0c255b782b894350d5229ba08865317c0391be13
WebPerformer <webperformerz@gmail.com>
2023-04-14 16:24:37 -0300

    formating CSS file and setup index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <div className="main-content">
+      <div className="img-gallery">
+        <div className="img-gallery-container">
+          <div className="img">
+            <img src="https://images.pexels.com/photos/15873635/pexels-photo-15873635.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+          </div>
+          <div className="img">
+            <img src="https://images.pexels.com/photos/10401968/pexels-photo-10401968.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+          </div>
+          <div className="img">
+            <img src="https://images.pexels.com/photos/15858941/pexels-photo-15858941.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+          </div>
+          <div className="img">
+            <img src="https://images.pexels.com/photos/14844108/pexels-photo-14844108.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+          </div>
+          <div className="img">
+            <img src="https://images.pexels.com/photos/10477018/pexels-photo-10477018.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="btn">Flip Layout</div>
+    </div>
+  )
+}
